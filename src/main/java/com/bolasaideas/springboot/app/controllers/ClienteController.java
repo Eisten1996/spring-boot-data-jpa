34ec3173bd5cc31afc7f8fbf17f1b34f16af8617
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-25 12:12:27 -0500

    Agregando editar

--- src/main/java/com/bolasaideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolasaideas/springboot/app/controllers/ClienteController.java
@@ -4,44 +4,69 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolasaideas.springboot.app.models.dao.IClienteDao;
 import com.bolasaideas.springboot.app.models.entities.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
-
+	Logger logger = LoggerFactory.getLogger(ClienteController.class);
 	@Autowired
 	private IClienteDao clienteDao;
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de Clientes");
+		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
 
 	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
+
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
-		model.put("titulo", "Formulario cliente");
+		model.put("titulo", "Formulario de Cliente");
+		return "form";
+	}
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+
+		Cliente cliente = null;
+
+		if (id > 0) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
 		return "form";
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		if (result.hasErrors()) {
-			model.addAttribute("titulo", "Formulario cliente");
+			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
 		}
+
 		clienteDao.save(cliente);
+		status.setComplete();
 		return "redirect:listar";
 	}
+
 }
