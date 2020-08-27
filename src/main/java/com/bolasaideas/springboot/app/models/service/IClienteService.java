da2ecc006d45cdf51a4bbc44b611c0f3cab3d08d
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-27 15:38:53 -0500

    Bonus track: Paginador desde cero - Usando PagingAndSortingRepository - Parte I

--- src/main/java/com/bolasaideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolasaideas/springboot/app/models/service/IClienteService.java
@@ -2,10 +2,15 @@ package com.bolasaideas.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolasaideas.springboot.app.models.entities.Cliente;
 
 public interface IClienteService {
 	public List<Cliente> findAll();
+
+	public Page<Cliente> findAll(Pageable pageable);
 
 	public void save(Cliente cliente);
 
