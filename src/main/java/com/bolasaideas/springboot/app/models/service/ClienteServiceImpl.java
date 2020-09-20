550f734693b6abe5c58f197758c669d60fb9c44c
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-20 16:28:07 -0500

    Consulta JPA para buscar productos en el Autocomplete

--- src/main/java/com/bolasaideas/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/bolasaideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -2,6 +2,8 @@ package com.bolasaideas.springboot.app.models.service;
 
 import java.util.List;
 
+import com.bolasaideas.springboot.app.models.dao.IProductoDao;
+import com.bolasaideas.springboot.app.models.entities.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,42 +16,51 @@ import com.bolasaideas.springboot.app.models.entities.Cliente;
 @Service
 public class ClienteServiceImpl implements IClienteService {
 
-	@Autowired
-	private IClienteDao clienteDao;
+    @Autowired
+    private IClienteDao clienteDao;
 
-	@Override
-	@Transactional(readOnly = true)
-	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
-		return (List<Cliente>) clienteDao.findAll();
-	}
+    @Autowired
+    private IProductoDao productoDao;
 
-	@Override
-	@Transactional
-	public void save(Cliente cliente) {
-		// TODO Auto-generated method stub
-		clienteDao.save(cliente);
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        // TODO Auto-generated method stub
+        return (List<Cliente>) clienteDao.findAll();
+    }
 
-	@Override
-	@Transactional(readOnly = true)
-	public Cliente findOne(Long id) {
-		// TODO Auto-generated method stub
-		return clienteDao.findById(id).orElse(null);
-	}
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        // TODO Auto-generated method stub
+        clienteDao.save(cliente);
+    }
 
-	@Override
-	@Transactional
-	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		clienteDao.deleteById(id);
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findOne(Long id) {
+        // TODO Auto-generated method stub
+        return clienteDao.findById(id).orElse(null);
+    }
 
-	@Override
-	@Transactional(readOnly = true)
-	public Page<Cliente> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return clienteDao.findAll(pageable);
-	}
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        // TODO Auto-generated method stub
+        clienteDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByNombre(String term) {
+        return productoDao.findByNombreLikeIgnoreCase("%" + term + "%");
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Cliente> findAll(Pageable pageable) {
+        // TODO Auto-generated method stub
+        return clienteDao.findAll(pageable);
+    }
 
 }
--- src/main/java/com/bolasaideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolasaideas/springboot/app/models/service/IClienteService.java
@@ -2,19 +2,22 @@ package com.bolasaideas.springboot.app.models.service;
 
 import java.util.List;
 
+import com.bolasaideas.springboot.app.models.entities.Producto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.bolasaideas.springboot.app.models.entities.Cliente;
 
 public interface IClienteService {
-	public List<Cliente> findAll();
+    public List<Cliente> findAll();
 
-	public Page<Cliente> findAll(Pageable pageable);
+    public Page<Cliente> findAll(Pageable pageable);
 
-	public void save(Cliente cliente);
+    public void save(Cliente cliente);
 
-	public Cliente findOne(Long id);
+    public Cliente findOne(Long id);
 
-	public void delete(Long id);
+    public void delete(Long id);
+
+    public List<Producto> findByNombre(String term);
 }
