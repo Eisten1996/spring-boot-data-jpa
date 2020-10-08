78caf1a70f9f63c99b64722d157a54b560b553ba
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-07 19:32:41 -0500

    Configurando y registrando JpaUserDetailsService

--- src/main/java/com/bolasaideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolasaideas/springboot/app/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bolasaideas.springboot.app;
 
 import com.bolasaideas.springboot.app.auth.handler.LoginSuccesHandler;
+import com.bolasaideas.springboot.app.models.service.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,8 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
 
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
@@ -19,35 +18,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    private DataSource dataSource;
-
-
-    @Autowired
     private LoginSuccesHandler successHandler;
 
     @Autowired
+    private JpaUserDetailsService userDetailsService;
+
+    @Autowired
     public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        builder.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder)
-                .usersByUsernameQuery("select username, password, enable from users where username=?")
-                .authoritiesByUsernameQuery("select u.username, a.authoritiy from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
-//        PasswordEncoder encoder = this.passwordEncoder;
-//        User.UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-//
-//        builder.inMemoryAuthentication()
-//                .withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-//                .withUser(users.username("dipper").password("12345").roles("USER"));
+        builder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/listar", "/images/**").permitAll()
-//                .antMatchers("/ver/**").hasAnyRole("USER")
-//                .antMatchers("/uploads/**").hasAnyRole("USER")
-//                .antMatchers("/form/**").hasAnyRole("ADMIN")
-//                .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-//                .antMatchers("/factura/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
