7e89316b3db014442397b4907c167b861d549201
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-12 18:44:03 -0500

    Implementando la clase ClienteCsvView y Link en vista listar clientes

--- src/main/java/com/bolasaideas/springboot/app/view/csv/ClienteCsvView.java
+++ src/main/java/com/bolasaideas/springboot/app/view/csv/ClienteCsvView.java
@@ -1,6 +1,12 @@
 package com.bolasaideas.springboot.app.view.csv;
 
+import com.bolasaideas.springboot.app.models.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +17,7 @@ import java.util.Map;
  * @project spring-boot-data-jpa
  * @created 12/10/2020 - 18:13
  */
+@Component("listar")
 public class ClienteCsvView extends AbstractView {
 
     public ClienteCsvView() {
@@ -29,5 +36,16 @@ public class ClienteCsvView extends AbstractView {
         httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"cliente.csv\"");
         httpServletResponse.setContentType(getContentType());
 
+        Page<Cliente> clientes = (Page<Cliente>) map.get("clientes");
+
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(httpServletResponse.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+        String[] header = {"id", "nombre", "apellido", "email", "createAt"};
+        beanWriter.writeHeader(header);
+
+        for (Cliente cliente : clientes) {
+            beanWriter.write(cliente, header);
+        }
+        beanWriter.close();
     }
 }
