8e6df47141a96d164952b2755c99301fc959afaf
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-12 12:10:23 -0500

    Mejorando el diseño y look and feel de la planilla Excel

--- src/main/java/com/bolasaideas/springboot/app/view/xlsx/FacturaXlsxView.java
+++ src/main/java/com/bolasaideas/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -2,10 +2,7 @@ package com.bolasaideas.springboot.app.view.xlsx;
 
 import com.bolasaideas.springboot.app.models.entities.Factura;
 import com.bolasaideas.springboot.app.models.entities.ItemFactura;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
@@ -24,6 +21,7 @@ public class FacturaXlsxView extends AbstractXlsxView {
     protected void buildExcelDocument(Map<String, Object> map, Workbook workbook,
                                       HttpServletRequest httpServletRequest,
                                       HttpServletResponse httpServletResponse) throws Exception {
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"factura_vew\"");
         Factura factura = (Factura) map.get("factura");
         Sheet sheet = workbook.createSheet("Factura Spring");
 
@@ -44,22 +42,57 @@ public class FacturaXlsxView extends AbstractXlsxView {
         sheet.createRow(6).createCell(0).setCellValue("Descripcion: " + factura.getDescripcion());
         sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
 
+        CellStyle theaderStyle = workbook.createCellStyle();
+        theaderStyle.setBorderBottom(BorderStyle.MEDIUM);
+        theaderStyle.setBorderTop(BorderStyle.MEDIUM);
+        theaderStyle.setBorderLeft(BorderStyle.MEDIUM);
+        theaderStyle.setBorderRight(BorderStyle.MEDIUM);
+        theaderStyle.setFillForegroundColor(IndexedColors.GOLD.index);
+        theaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        CellStyle tbodyStyle = workbook.createCellStyle();
+        tbodyStyle.setBorderBottom(BorderStyle.THIN);
+        tbodyStyle.setBorderTop(BorderStyle.THIN);
+        tbodyStyle.setBorderLeft(BorderStyle.THIN);
+        tbodyStyle.setBorderRight(BorderStyle.THIN);
+
         Row header = sheet.createRow(9);
         header.createCell(0).setCellValue("Producto");
         header.createCell(1).setCellValue("Precio");
         header.createCell(2).setCellValue("Cantidad");
         header.createCell(3).setCellValue("Total");
 
+        header.getCell(0).setCellStyle(theaderStyle);
+        header.getCell(1).setCellStyle(theaderStyle);
+        header.getCell(2).setCellStyle(theaderStyle);
+        header.getCell(3).setCellStyle(theaderStyle);
+
         int rownum = 10;
         for (ItemFactura itemFactura : factura.getItems()) {
             Row fila = sheet.createRow(rownum++);
-            fila.createCell(0).setCellValue(itemFactura.getProducto().getNombre());
-            fila.createCell(1).setCellValue(itemFactura.getProducto().getPrecio());
-            fila.createCell(2).setCellValue(itemFactura.getCantidad());
-            fila.createCell(3).setCellValue(itemFactura.calcularImporte());
+            cell = fila.createCell(0);
+            cell.setCellValue(itemFactura.getProducto().getNombre());
+            cell.setCellStyle(tbodyStyle);
+
+            cell = fila.createCell(1);
+            cell.setCellValue(itemFactura.getProducto().getPrecio());
+            cell.setCellStyle(tbodyStyle);
+
+            cell = fila.createCell(2);
+            cell.setCellValue(itemFactura.getCantidad());
+            cell.setCellStyle(tbodyStyle);
+
+            cell = fila.createCell(3);
+            cell.setCellValue(itemFactura.calcularImporte());
+            cell.setCellStyle(tbodyStyle);
         }
         Row filaTotal = sheet.createRow(rownum);
-        filaTotal.createCell(2).setCellValue("Gran total: ");
-        filaTotal.createCell(3).setCellValue(factura.getTotal());
+        cell = filaTotal.createCell(2);
+        cell.setCellValue("Gran total: ");
+        cell.setCellStyle(tbodyStyle);
+
+        cell = filaTotal.createCell(3);
+        cell.setCellValue(factura.getTotal());
+        cell.setCellStyle(tbodyStyle);
     }
 }
